c103c82a60dbeac2203e1434b437c44fd0bbddae
João Pedro Melquiades Gomes <joao.gomes@embedded.ufcg.edu.br>
2019-12-17 11:30:32 -0300

    Retirados os arquivos de simulação

--- decoder/tb/xcelium.d/run.lnx8664.19.09.d/files.ts
+++
@@ -1,4 +0,0 @@
-1576587487 /mnt/hdd1tera/xmen/Documentos/joao.gomes/goulomb_uvm/decoder/tb/pkg.sv
-1576587487 /mnt/hdd1tera/xmen/Documentos/joao.gomes/goulomb_uvm/decoder/tb/dec_interface_if.sv
-1576587487 /mnt/hdd1tera/xmen/Documentos/joao.gomes/goulomb_uvm/decoder/rtl/exp_golomb_decoder.sv
-1576587487 /mnt/hdd1tera/xmen/Documentos/joao.gomes/goulomb_uvm/decoder/tb/top.sv
